46f3b44cbfbf2e80127430a005edc64a543f0f48
Nugine <Nugine@163.com>
2018-02-07 23:04:31 +0800

    info

---
+++ README.md
@@ -0,0 +1,5 @@
+# AsyncSpider
+
+## Async Spider Framework
+
+## Development Status :: 2 - Pre-Alpha
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="AsyncSpider",
+    version="0.1.0",
+    description="Async Spider Framework",
+    author="Nugine",
+    author_email="Nugine@163.com",
+    url='https://github.com/Nugine/AsyncSpider',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        'aiohttp',
+    ],
+    license='Apache License',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
+)
